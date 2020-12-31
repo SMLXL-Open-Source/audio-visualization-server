1cf76d731765d5332defb7bf14ae48a8b7d01b67
khabubundivhu <khabubundivh@gmail.com>
2020-12-31 23:32:27 +0200

    cors middleware added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
